fcfd9843aebf71df9f3192a5474e38a65ca9155a
Edward Pinkston <edward.pinkston@gmail.com>
2023-09-29 21:31:51 -0500

    Adjust deadzone

--- src/EmulationWindow.cpp
+++ src/EmulationWindow.cpp
@@ -642,13 +642,14 @@ void EmulationWindow::Loop() {
                 if (emu->JoystickButtonHeld(Emulator::ControllerButton::B))
                     bus->SetController1(bus->GetController1() | (int)Bus::NESButtons::B);
 
-                if (SDL_JoystickGetAxis(emu->GetJoystick(), 0) < -16384)
+                // Only register joystick input if more than 25% of the way
+                if (SDL_JoystickGetAxis(emu->GetJoystick(), 0) < -16384/2)
                     bus->SetController1(bus->GetController1() | (int)Bus::NESButtons::LEFT);
-                else if (SDL_JoystickGetAxis(emu->GetJoystick(), 0) > 16384)
+                else if (SDL_JoystickGetAxis(emu->GetJoystick(), 0) > 16384/2)
                     bus->SetController1(bus->GetController1() | (int)Bus::NESButtons::RIGHT);
-                if (SDL_JoystickGetAxis(emu->GetJoystick(), 1) < -16384)
+                if (SDL_JoystickGetAxis(emu->GetJoystick(), 1) < -16384/2)
                     bus->SetController1(bus->GetController1() | (int)Bus::NESButtons::UP);
-                else if (SDL_JoystickGetAxis(emu->GetJoystick(), 1) > 16384)
+                else if (SDL_JoystickGetAxis(emu->GetJoystick(), 1) > 16384/2)
                     bus->SetController1(bus->GetController1() | (int)Bus::NESButtons::DOWN);
             }
         }
